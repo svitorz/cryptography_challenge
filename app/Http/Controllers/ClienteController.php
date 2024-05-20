c640afe09d8f5db98b551eccead9739e27892113
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-05-20 19:46:12 -0300

    get requests in ClienteController and Cliente Migration

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use App\Models\Cliente;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use PhpParser\Node\Expr\Cast\String_;
 
 class ClienteController extends Controller
 {
-    //
+    public function index(): Collection
+    {
+        return Cliente::all();
+    }
+
+    public function store(Request $request): void
+    {
+        $cliente = new Cliente;
+        $validated = $request->validate([
+            'userDocument' => ['required', 'numeric'],
+            'creditCardToken' => ['required', 'numeric'],
+            'value' => ['required', 'numeric'],
+        ]);
+
+        $cliente::create([
+            'userDocument' => Hash::make($validated['userDocument']),
+            'creditCardToken' => Hash::make($validated['creditCardToken']),
+            'value' => $validated['value'],
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        return Cliente::findOrFail($id);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(ClienteController::class)->group(function () {
+    Route::get('/sensitive-data', 'index')->name('index');
+    Route::get('/sensitive-data/{id}', 'show')->name('index');
+
 });
