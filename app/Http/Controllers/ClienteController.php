a3edc3fd69f20be9622de1750220d70d5d6ff872
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-05-20 22:26:35 -0300

    finish project

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\Models\Cliente;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Hash;
 use PhpParser\Node\Expr\Cast\String_;
 
@@ -16,7 +17,7 @@ class ClienteController extends Controller
         return Cliente::all();
     }
 
-    public function store(Request $request): void
+    public function store(Request $request)
     {
         $cliente = new Cliente;
         $validated = $request->validate([
@@ -25,7 +26,7 @@ class ClienteController extends Controller
             'value' => ['required', 'numeric'],
         ]);
 
-        $cliente::create([
+        return $cliente::Create([
             'userDocument' => Hash::make($validated['userDocument']),
             'creditCardToken' => Hash::make($validated['creditCardToken']),
             'value' => $validated['value'],
@@ -34,7 +35,30 @@ class ClienteController extends Controller
 
     public function show(int $id)
     {
-        return Cliente::findOrFail($id);
+        $cliente = Cliente::find($id);
+        $cliente['edit'] = route('edit', ['id' => $id]);
+        $cliente['destroy'] = route('destroy', ['id' => $id]);
+        return $cliente;
     }
 
-}
+    public function edit(int $id)
+    {
+        return Cliente::find($id);
+    }   
+
+    public function update(int $id, Request $request)
+    {
+        $cliente = Cliente::findOrFail($id);
+        $validated = $request->validate([
+            'value' => ['required', 'numeric'],
+        ]);
+        $cliente->value = $validated['value'];
+        return $cliente->save();
+    }
+
+    public function destroy(int $id)
+    {
+        $cliente = Cliente::findOrFail($id);
+        return $cliente->delete();
+    }
+}  
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::controller(ClienteController::class)->group(function () {
     Route::get('/sensitive-data', 'index')->name('index');
-    Route::get('/sensitive-data/{id}', 'show')->name('index');
-
+    Route::get('/sensitive-data/show/{id}', 'show')->name('show');
+    Route::get('/sensitive-data/edit/{id}', 'edit')->name('edit');
+    
+    Route::post('/sensitive-data/store','store')->name('store');
+    Route::post('/sensitive-data/update/{id}', 'update')->name('update');    
+    Route::delete('/sensitive-data/destroy/{id}','destroy')->name('destroy');
 });
